0c33a67beb284818c2901ab69ce71d3d6e750a7d
alvarito1999 <oj.alv99@gmail.com>
2024-07-07 17:33:35 -0300

    player prototype

--- js/game.js
+++ js/game.js
@@ -1,3 +1,119 @@
+class Player {
+  constructor(ID, name, gun, shootKey = "a", reloadKey = "s", blockKey = "d") {
+    this.ID = ID;
+    this.name = name;
+    this.gun = gun;
+    this.life = 100;
+    this.shootKey = shootKey;
+    this.reloadKey = reloadKey;
+    this.blockKey = blockKey;
+    this.isBlocking = false;
+    this.shootHandlerFunction = null;
+    this.reloadHandlerFunction = null;
+    this.blockHandlerFunction = null;
+  }
+  // shoot
+  shoot(enemy) {
+    this.isBlocking = false;
+    if (this.gun.bullets > 0) {
+      //reduce enemy life equal to gun damage if enemy is not blocking and you are'nt out of bullets
+      if (!enemy.isBlocking) {
+        this.gun.bullets -= 1;
+        console.log(`${this.name} bullets: ${this.gun.bullets}`);
+        enemy.life -= this.gun.damage;
+        console.log(`${enemy.name} life: ${enemy.life}`);
+      } else {
+        //if the enemy is blocking you dont deal damage
+        console.log(`${this.name} shoots but ${enemy.name} is blocking`);
+      }
+    } else {
+      //if you are out of bullets, you don't deal damage
+      console.log(`${this.name} is out of bullets!`);
+    }
+  }
+
+  shootHandler(enemy) {
+    const sh = (e) => {
+      if (e.key === this.shootKey) {
+        this.shoot(enemy);
+      }
+    };
+    return sh;
+  }
+  addShootListener(enemy) {
+    this.shootHandlerFunction = this.shootHandler(enemy);
+    document.addEventListener("keydown", this.shootHandlerFunction);
+  }
+  removeShootListener() {
+    document.removeEventListener("keydown", this.shootHandlerFunction);
+    this.shootHandlerFunction = null;
+  }
+  // reload
+  reload() {
+    this.isBlocking = false;
+    if (this.gun.bullets < this.gun.magazineSize) {
+      console.log(`${this.name}: +1 bullet`);
+      this.gun.bullets += 1;
+    } else {
+      console.log(`${this.name}: full ammo`);
+    }
+  }
+  reloadHandler() {
+    const sh = (e) => {
+      if (e.key === this.reloadKey) {
+        this.reload();
+      }
+    };
+    return sh;
+  }
+  addReloadListener() {
+    this.reloadHandlerFunction = this.reloadHandler();
+    document.addEventListener("keydown", this.reloadHandlerFunction);
+  }
+  removeReloadListener() {
+    document.removeEventListener("keydown", this.reloadHandlerFunction);
+    this.reloadHandlerFunction = null;
+  }
+  //block
+  block() {
+    //while the block button is pressing the state of "blocking" is true
+    //during the this state you are invulneable
+    //this state has a counter what reduces while you blocking and recharges while you don't
+    //if the counter of blocking is equal to zero, you loose blocking for a moment
+    console.log(`${this.name} is blocking!`);
+
+    this.isBlocking = true;
+  }
+  blockHandler() {
+    const sh = (e) => {
+      if (e.key === this.blockKey) {
+        this.block();
+      }
+    };
+    return sh;
+  }
+  addBlockListener() {
+    this.blockHandlerFunction = this.blockHandler();
+    document.addEventListener("keydown", this.blockHandlerFunction);
+  }
+  removeBlockListener() {
+    document.removeEventListener("keydown", this.blockHandlerFunction);
+    this.blockHandlerFunction = null;
+  }
+}
+class Gun {
+  constructor(name, damage, bullets, magazineSize) {
+    this.name = name;
+    this.damage = damage;
+    this.bullets = bullets;
+    this.magazineSize = magazineSize;
+  }
+}
+const revolver1 = new Gun("revolver", 50, 6, 6);
+const revolver2 = new Gun("revolver", 1, 6, 6);
+const p1 = new Player(1, "alvaro", revolver1);
+const p2 = new Player(1, "juan", revolver2, "z", "x", "c");
+
 class Game {
   constructor() {
     this.globalHandlerFunction = null;
